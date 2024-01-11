00f9645655df029a0f24aa64f707036882ff495b
Andry Ariadi <andryariadi23@gmail.com>
2024-01-12 05:53:32 +0700

    setup next js

---
+++ clone-ui/src/app/layout.js
@@ -0,0 +1,21 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata = {
+  title: "Bakmi GM",
+  description: "Clone UI Bakmi GM",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+    </html>
+  );
+}

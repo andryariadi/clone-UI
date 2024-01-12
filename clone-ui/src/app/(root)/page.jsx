459dd5e6d593a6bba0c7d07a17a70349133a8db1
Andry Ariadi <andryariadi23@gmail.com>
2024-01-12 10:34:35 +0700

    styling point component

--- clone-ui/src/app/(root)/page.jsx
+++ clone-ui/src/app/(root)/page.jsx
@@ -1,10 +1,12 @@
 import Level from "@/components/Level";
+import Point from "@/components/Point";
 
 export default function Home() {
   return (
     <>
-      <section className="wrapper">
+      <section className="wrapper flex flex-col gap-8">
         <Level />
+        <Point />
       </section>
     </>
   );
---
+++ clone-ui/src/components/Point.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+export default function Point() {
+  return (
+    <>
+      <div className="shadow-md py-5 px-5 md:px-10 flex flex-col gap-1">
+        <div className="flex justify-between">
+          <h1 className="font-bold text-lg md:text-2xl">Bakmi GM Points</h1>
+          <p className="font-bold text-lg md:text-2xl">40</p>
+        </div>
+        <div>
+          <p className="text-gray-500 text-[11px] md:text-lg font-medium">
+            <span className="font-bold">40</span> will expire on <span className="font-bold">22 Feb 2024</span>.{" "}
+            <Link href="/" className="font-bold text-sky-500">
+              See Deails
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
